1bee38eda64582d5d99b64c7c604b1124bff8510
Abdul Syed <akasim123@gmail.com>
2019-02-24 14:59:03 -0800

    Takes input of a word and returns whether it is a bad word

--- profanity-filter-akasim910/profanity_filter.py
+++ profanity-filter-akasim910/profanity_filter.py
@@ -1,8 +1,38 @@
 def main():
-	file = open("bad-words.txt", 'r')
-	row = file.readlines()
-	for line in row:
-		print(line)
+	file = open("bad-words.txt", 'r') 
+	file1 = file.readlines()
+
+
+
+	#string that will be looked for
+	temp = input("Enter the word that you want to search: ")
+
+	#add /n to end of string and convert it to a list
+	temp += "\n"
+	temp = [temp]
+
+
+	#get any matches
+	result = set(file1).intersection(temp)
+	result = list(result)
+
+	if len(result) > 0:
+		print("**************************************")
+		print("    YOU HAVE ENTERED A BAD WORD     ")
+		print("**************************************")
+		#prints what word matches
+		print("The bad word is: ")
+		print(result[0][:-1])
+	else:
+		print("************************")
+		print("    NOT A BAD WORD     ")
+		print("************************")
+
+
+	
+
+
+
 
 
 
