017d48c05d2c884baff9b09f30e985cab9f87c27
Abdul Syed <akasim123@gmail.com>
2019-02-24 17:21:33 -0800

    added word censoring capabilities

--- profanity-filter-akasim910/profanity_filter.py
+++ profanity-filter-akasim910/profanity_filter.py
@@ -1,20 +1,34 @@
-def main():
+def censor_word(temp):
+	new_str = ""
+	s = ['$','#','@','*','&','%']
+	new_str += temp[0]
+	for i in range(len(temp) - 2):
+		new_str += random.choice(s)
+	new_str += temp[-1]
+	return new_str
+	
+
+
+
+def check_if_bad_word(temp):
 	file = open("bad-words.txt", 'r') 
 	file1 = file.readlines()
 
+	##string that will be looked for
+	##if you want to test input
 
-
-	#string that will be looked for
-	temp = input("Enter the word that you want to search: ")
+	#temp = input("Enter the word that you want to search: ")
 
 	#add /n to end of string and convert it to a list
 	temp += "\n"
 	temp = [temp]
 
-
 	#get any matches
 	result = set(file1).intersection(temp)
 	result = list(result)
+
+	#set bool value to default
+	bool_val = None
 
 	if len(result) > 0:
 		print("**************************************")
@@ -23,20 +37,18 @@ def main():
 		#prints what word matches
 		print("The bad word is: ")
 		print(result[0][:-1])
+		bool_val = True
 	else:
 		print("************************")
 		print("    NOT A BAD WORD     ")
 		print("************************")
-
-
-	
-
-
-
-
+		bool_val = False
 
 
 
 
 if __name__ == '__main__':
-	main()
+	import random
+	temp = "ass"
+	check_if_bad_word(temp)
+	print(censor_word(temp))
